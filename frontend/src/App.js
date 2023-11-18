7ceb9733b4c687709e5352c23c43abae64045bd5
psman <samuelpo49@icloud.com>
2023-11-17 20:04:27 -0700

    created Navbar and set up Tailwind CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,12 @@
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-const Home = () => {
-  <div>
-    <h2>Home</h2>
-  </div>
-};
-
-const Course = () => {
-  <div>
-    <h2>Course</h2>
-  </div>
-};
-
-const Edpresso = () => {
-  <div>
-    <h2>Edpresso</h2>
-  </div>
-};
-
-function App() {
+const App = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/course">Course</Link>
-          </li>
-          <li>
-            <Link to="/edpresso">Edpresso</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/course" element={<Course />} />
-        <Route path="/edpresso" element={<Edpresso />} />
-      </Routes>
+      <Navbar auth={false} />
     </>
   );
-}
+};
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
-import App from './App';
-
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
